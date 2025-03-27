7c6b02195991a0349627942fc521b5dd71a278d8
Octavio <octavioand@gmail.com>
2025-03-27 18:47:00 +0000

    CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,18 +2,84 @@
 
 int main(){
 
-    char carta1[0] = "Carta 1";
+    char carta1[] = "Carta 1";
+    char codigo1[] = "C01";
+    char cidade1[] = "Fortaleza";
     char estado1 = 'C';
-    char cidade1[0] = "Fortaleza";
-    char codigo1 = "C01";
     int populacao1 = 2430000;
     float pib1 = 65.16;
     float area1 = 312.353;
     int pontosturisticos1 = 61; 
 
-    scanf("%s", &carta1);
-    printf("Carta 1: %s\n");
+    scanf("%s", carta1);
+    printf("Carta 1 \n");
+    printf("Código: %s\n", codigo1);
+    printf("Cidade: %s\n", cidade1);
+    printf("Estado: %c\n", estado1);
+    printf("População: %d\n", populacao1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("Área: %.3f km² \n", area1);
+    printf("Pontos Turísticos: %d\n", pontosturisticos1);
+    printf("\n");
 
+    char carta2[] = "Carta 2";
+    char codigo2[] = "E02";
+    char cidade2[] = "Vitória";
+    char estado2 = 'E';
+    int populacao2 = 322869;
+    float pib2 = 3.15;
+    float area2 = 97.123;
+    int pontosturisticos2 = 50; 
+
+    scanf("%s", carta2);
+    printf("Carta 2 \n");
+    printf("Código: %s\n", codigo2);
+    printf("Cidade: %s\n", cidade2);
+    printf("Estado: %c\n", estado2);
+    printf("População: %d\n", populacao2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Área: %.3f km²\n", area2);
+    printf("Pontos Turísticos: %d\n", pontosturisticos2);
+    printf("\n");
+
+    char carta3[] = "Carta 3";
+    char codigo3[] = "B03";
+    char cidade3[] = "Salvador ";
+    char estado3 = 'B';
+    int populacao3 = 2430000;
+    float pib3 = 62.95;
+    float area3 = 303.34;
+    int pontosturisticos3 = 45; 
+
+    scanf("%s", carta3);
+    printf("Carta 3 \n");
+    printf("Código: %s\n", codigo3);
+    printf("Cidade: %s\n", cidade3);
+    printf("Estado: %c\n", estado3);
+    printf("População: %d\n", populacao3);
+    printf("PIB: %.2f bilhões de reais\n", pib3);
+    printf("Área: %.3f km²\n", area3);
+    printf("Pontos Turísticos: %d\n", pontosturisticos3);
+    printf("\n");
+
+    char carta4[] = "Carta 4";
+    char codigo4[] = "A04";
+    char cidade4[] = "Manaus";
+    char estado4 = 'A';
+    int populacao4 = 2060000;
+    float pib4 = 103.2;
+    float area4 = 11401.09;
+    int pontosturisticos4 = 40; 
+
+    scanf("%s", carta4);
+    printf("Carta 4 \n");
+    printf("Código: %s\n", codigo4);
+    printf("Cidade: %s\n", cidade4);
+    printf("Estado: %c\n", estado4);
+    printf("População: %d\n", populacao4);
+    printf("PIB: %.2f bilhões de reais\n", pib4);
+    printf("Área: %.3f km²\n", area4);
+    printf("Pontos Turísticos: %d\n", pontosturisticos4);
 
 
 
